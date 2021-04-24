14f9392ec50c7c2fb9af56d8b45bdf4faba0051a
mjk1252 <mjk.visser@gmail.com>
2021-04-24 15:40:52 +0200

    First Commit

---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function eventPage() {
+  return (
+    <div>
+      <h1>My Event</h1>
+    </div>
+  )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function eventsPage() {
+  return (
+    <div>
+      <h1>My Events</h1>
+    </div>
+  )
+}
