c9f84b964917ac884c3efe940436f1778d515320
mjk1252 <mjk.visser@gmail.com>
2021-04-25 10:35:46 +0200

    Add Styles, Add React Icons 'Very Nice', Folder Aliasing

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Layout.module.css'
+import styles from '@/styles/Layout.module.css'
 import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // import styles from '../styles/Home.module.css'
 // import Head from 'next/head'
-import Layout from '../components/Layout'
+import Layout from '@/components/Layout'
 
 export default function HomePage() {
   return (
